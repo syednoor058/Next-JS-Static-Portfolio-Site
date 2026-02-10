92244ddaf5c340f6f1013715f274e1b45da7d2f2
Shaed Noor <syednoor058@gmail.com>
2026-02-10 12:32:48 +0600

    Hero section image changed.

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -17,14 +17,17 @@ const items = [
   "Redux",
   "Zustand",
   "JEST",
-  "Shad/CN"
+  "Shad/CN",
 ];
 
 const Hero = () => {
   return (
     <section className="w-full min-h-screen flex flex-col relative text-primary pt-10 bg-black overflow-hidden">
       {/* Background Cover */}
-      <FadeIn delay={0.1} className="absolute inset-0 w-full h-full z-0 blur-[0px]">
+      <FadeIn
+        delay={0.1}
+        className="absolute inset-0 w-full h-full z-0 blur-[0px]"
+      >
         <Image
           src="https://res.cloudinary.com/de8g5laai/image/upload/v1770377281/26127101_wavy_background_4_buejwi.jpg"
           alt="a dark hero background"
@@ -51,10 +54,11 @@ const Hero = () => {
         <span className="invisible text-end full-width-word text-white font-bold leading-tight">
           ENGNR.
         </span>
+
         <span
           className="absolute inset-0 z-10 text-end full-width-word text-transparent font-bold leading-tight"
           style={{
-            WebkitTextStroke: "1px white",
+            WebkitTextStroke: "2px white",
           }}
         >
           ENGNR.
@@ -67,20 +71,22 @@ const Hero = () => {
       >
         <div className="h-[70%] lg:h-[85%] aspect-711/841 ">
           <Image
-          src="https://res.cloudinary.com/de8g5laai/image/upload/v1770266190/ChatGPT_Image_Feb_4_2026_09_14_51_PM-Photoroom_diyiou.png"
-          alt="syed shaeduzzaman noor photo"
-          height={841}
-          width={711}
-          className="object-cover"
-        />
+            src="https://res.cloudinary.com/de8g5laai/image/upload/v1770703131/Untitled_design_5_-Photoroom_zvnyxy.png"
+            alt="a robot wearing an engineer cap"
+            height={841}
+            width={711}
+            className="object-cover"
+          />
         </div>
       </FadeIn>
 
       <div className="w-full  pt-0 absolute bottom-0 z-20 flex flex-col gap-10 lg:gap-14">
         <div className="px-4 md:px-6 lg:px-0 lg:pl-6 max-w-110 relative z-1">
           <FadeIn delay={0.7}>
-            <p className="max-w-2xl mb-8 text-lg lg:text-xl">
-              Full-stack software engineer building scalable, performance-driven web applications—focused on clean architecture and real-world impact.
+            <p className="max-w-2xl mb-8 text-lg lg:text-xl font-light">
+              Full-stack software engineer building scalable, performance-driven
+              web applications—focused on clean architecture and real-world
+              impact.
             </p>
           </FadeIn>
           <FadeIn delay={0.8}>
